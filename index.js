bf17c0951afd833b0679bdb47d4a7b0132549bdb
Gaurav Pratap Singh <gs492979@gmail.com>
2025-04-10 12:16:07 +0530

    negative value of day and month fixed.

--- index.js
+++ index.js
@@ -8,13 +8,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {
+    Day: "--",
+    Month: "--",
+    Year: "--",
+    userDay: null,
+    userMonth: null,
+    userYear: null,
+  });
 });
 
 app.post("/", (req, res) => {
   const currentDate = new Date();
   const currentDay = currentDate.getDate();
-  const currentMonth = currentDate.getMonth();
+  const currentMonth = currentDate.getMonth() + 1;
   const currentYear = currentDate.getFullYear();
 
   // Data entered by the user
@@ -24,10 +31,31 @@ app.post("/", (req, res) => {
   const userYear = Number(year);
 
   // Difference in Day, Month, and Year
-  const diffDay = userDay - currentDay;
-  const diffMonth = userMonth - currentMonth;
-  const diffYear = userYear - currentYear;
-  res.render("index.ejs", { Day: diffDay, Month: diffMonth, Year: diffYear });
+  let diffDay = currentDay - userDay;
+  let diffMonth = currentMonth - userMonth;
+  let diffYear = currentYear - userYear;
+
+  if (diffDay < 0) {
+    diffMonth -= 1;
+    const daysInPrevMonth = new Date(
+      currentYear,
+      currentMonth - 1,
+      0
+    ).getDate();
+    diffDay += daysInPrevMonth;
+  }
+  if (diffMonth < 0) {
+    diffYear -= 1;
+    diffMonth += 12;
+  }
+  res.render("index.ejs", {
+    Day: diffDay,
+    Month: diffMonth,
+    Year: diffYear,
+    userDay: userDay,
+    userMonth: userMonth,
+    userYear: userYear,
+  });
 });
 app.listen(port, () => {
   console.log(`App started listening on port ${port}`);
